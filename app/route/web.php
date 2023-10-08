eb1191d4b21448d830d9b03ea7400fe36644cb02
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2023-10-08 18:08:55 -0300

    finalização

--- app/route/web.php
+++ app/route/web.php
@@ -8,18 +8,8 @@ use App\Middleware\JwtMiddleware;
 SimpleRouter::group(['users' => JwtMiddleware::class], function () {
     SimpleRouter::get('/',  [UserController::class, 'home']);
 });
-
-
 SimpleRouter::post('/auth', [AuthController::class, 'auth']);
-
-
-SimpleRouter::get('/debug', function(){
-
-    phpinfo();
-
-});
-
-
+SimpleRouter::get('/debug', function(){ phpinfo();});
 SimpleRouter::get('/favicon.ico', function() {
     return;
 });
