9edf45c71976d9a1ff8e482e41d2bf8fa018163f
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2023-11-15 20:34:22 -0300

    create metodo upload file

--- app/Controllers/UploadFileController.php
+++ app/Controllers/UploadFileController.php
@@ -6,22 +6,24 @@ use App\Helpers\Header;
 use App\Utils\UploadUtil;
 use Exception;
 
- class UploadFileController extends BaseController{
-   public function createUpload() {
-     try {
-    
-       $requestFile = $this->request->getUploadedFiles();
-       $uploadedFile = $requestFile['file'];
-       if (!empty($uploadedFiles['file'])) {
-         return Header::validateRequest((int)400, 'Por favor selecinar um file');
-        }else{ 
-       $uploadedFile->moveTo(__DIR__ .'./../../uploads/' . $uploadedFile->getClientFilename());
-     return UploadUtil::sendFile($uploadedFile->getClientFilename());
+class UploadFileController extends BaseController
+{
+  public function createUpload()
+  {
+    try {
+
+      $requestFile = $this->request->getUploadedFiles();
+      $uploadedFile = $requestFile['file'];
+      if (!empty($uploadedFiles['file'])) {
+        return Header::validateRequest((int) 400, 'Por favor selecinar um file');
+      } else {
+        $uploadedFile->moveTo(__DIR__ . './../../uploads/' . $uploadedFile->getClientFilename());
+        return UploadUtil::sendFile($uploadedFile->getClientFilename());
+      }
+
+    } catch (Exception $e) {
+      return Header::validateRequest((int) 500, $e->getMessage());
     }
 
-     } catch (Exception $e) {
-     return Header::validateRequest((int)500, $e->getMessage());
-     }
-
-}
+  }
 }
--- app/Utils/UploadUtil.php
+++ app/Utils/UploadUtil.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Utils;
-
-use ImageKit\ImageKit;
 use App\Helpers\Header;
 use Exception;
+;
+
+
 
 class UploadUtil
 {
@@ -11,35 +12,32 @@ class UploadUtil
     public static function sendFile($namefile)
     {
         try {
-            $imageKit = new ImageKit(
-                $_ENV['PUBLIC_KEY'],
-                $_ENV['PRIVATE_KEY'],
-                $_ENV['URL_ENDPOINT']
-            );
-
-            $filePath = __DIR__. './../../uploads/'.$namefile;
-            $base64 = base64_encode(file_get_contents($filePath));
-                        
-            $file = $imageKit->upload([
-                'file' => $base64,
-                'fileName' => $namefile,
-                "folder" => "/teste",
-                "tags" => ['tag1']
+           $client= GuzzHttp::ClientHttp();
+            $filePath =  './uploads/'.$namefile;
+            $base64Image = base64_encode(file_get_contents($filePath));
+            if (file_exists($filePath)) {
+               $response = $client->request('POST', 'files/upload/', [
+                'multipart' => [
+                    [
+                        'name'     => 'file',
+                        'contents' => $base64Image,
+                        'headers'  => ['Content-Type' => 'application/json'],
+                       
+                    ],
+                    [
+                        'name'     => 'fileName',
+                        'contents' => $namefile,
+                    ],
+                ],
             ]);
-
-            var_dump($file);
-           // return Header::validateRequest((int)201, 'Teste');
-
-            // if ($file->error) {
-            //     var_dump($file->error);
-            //     $error = json_encode($file->error);
-            //    throw new Exception($error);
-            // } else {
-            //     return Header::validateRequest((int)201, 'Teste');
-            // }
-
+            $statusCode = $response->getStatusCode();
+            $body = json_decode($response->getBody()->getContents());
+            unlink($filePath);
+            return Header::validateRequest($statusCode, $body);
+            } else {
+                return Header::validateRequest((int) 204, 'arquivo não encontrado');
+            }
         } catch (Exception $th) {
-            var_dump($th);  
             return Header::validateRequest((int) 500, 'Erro durante o upload: ' . $th->getMessage());
         }
     }
