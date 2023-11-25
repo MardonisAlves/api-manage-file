d49395577c3aef3cc4bdd7d1c5b448edadaa9813
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2023-11-25 12:27:16 -0300

    develop

--- app/Model/User.php
+++ app/Model/User.php
@@ -8,7 +8,6 @@ class User extends Model {
     protected $table = 'User';
     protected $fillable = ['name', 'email','typeuser', 'password'];
 
-    
     public function endress(): HasOne{
         return $this->hasOne(Endress::class, 'userId');
     }
